857f0f52f99f8beb8d776856a49a0286b4e20824
Aranyak Panigrahi <Aranyak Panigrahi>
2024-04-19 17:32:25 +0530

    init

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+from dotenv import load_dotenv
+import json
+
+load_dotenv() ## load all our environment variables
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_repsonse(input):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content(input)
+    return response.text
+
+def input_pdf_text(uploaded_file):
+    reader=pdf.PdfReader(uploaded_file)
+    text=""
+    for page in range(len(reader.pages)):
+        page=reader.pages[page]
+        text+=str(page.extract_text())
+    return text
+
+#Prompt Template
+
+input_prompt="""
+Compose a complete cover letter for the position of using resume and job description from below, incorporating a strong introduction,
+a summary of relevant skills and experiences, an explanation of how my skills match the job requirements, a highlight
+of my most significant achievement, showcasing my adaptability and willingness to learn, demonstrating my passion for the industry, emphasizing my strong work ethic
+and commitment, mentioning the value of my professional network, addressing the company’s values and culture,
+and ending with a powerful conclusion that expresses my eagerness for an interview.
+resume:{text}
+description:{jd}
+
+I want the response in one single string having the structure
+{{"Cover Letter":""}}
+"""
+
+## streamlit app
+st.title("Smart ATS")
+st.text("Improve Your Resume ATS")
+jd=st.text_area("Paste the Job Description")
+uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
+
+submit = st.button("Submit")
+
+if submit:
+    if uploaded_file is not None:
+        text=input_pdf_text(uploaded_file)
+        response=get_gemini_repsonse(input_prompt)
+        st.subheader(response)
